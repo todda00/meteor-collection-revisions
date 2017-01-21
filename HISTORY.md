61f39875a378d46664fee1d9174a0e834578859d
Nick Lozon <nicklozon@gmail.com>
2017-01-21 14:10:37 -0500

    Tagging as 0.3.0. Setting package.js to assume myself as owner.

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,10 @@
 HISTORY.md
 
+### v0.3.0
+  - Fixed bug for restoring revision when revisions field is not set for collection
+  - Added a revision prune feature which will remove restored revision and all revisions after it
+  - Added feature for callbacks
+
 ### v0.2.1
   - Add required dependencies to package.js
 
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'todda00:collection-revisions',
-  version: '0.2.1',
+  name: 'nicklozon:collection-revisions',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'Keep revision history for collection documents and provide restore functionality.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/todda00/meteor-collection-revisions.git',
+  git: 'https://github.com/nicklozon/meteor-collection-revisions.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
